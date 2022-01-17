6f3b08e3f4d93085a8576c4fcf36315990f26d8e
kmzpius <piuskk71@gmail.com>
2022-01-17 12:33:56 +0300

    removed credentials

--- client.py
+++ client.py
@@ -11,7 +11,7 @@ i2c = SoftI2C (scl=Pin(22), sda=Pin(21), freq=400000)
 lcd = I2cLcd(i2c, DEFAULT_I2C_ADDR, 4, 16)
 
 def sendSample(sample):
-    with uwebsockets.client.connect('ws://137.184.24.216:7000/publish') as websocket:
+    with uwebsockets.client.connect('ws://xxx.xxx.xx.xxx:xxxxx/xxxxx') as websocket:
         lcd.putstr("Diagnosing please wait ....")
 
         encoded_sample_data = ujson.dumps(sample)
@@ -25,7 +25,6 @@ def sendSample(sample):
         lcd.clear()        
         lcd.move_to(2, 1)
         lcd.putstr("{}".format(LCD_message))
-#sample = {"Topic":"DV00023","message":'{"gas1":"345PH","gas2":"345PH","gas3":"345PH","gas4":"345PH","gas5":"345PH","gas6":"345PH"}'}
-#sendSample(sample)
+
 
 
